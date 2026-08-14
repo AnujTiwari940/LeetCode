7440c675a88648837db11b7e3afac12474d082dd
Anuj Tiwari <tiwarianuj940@gmail.com>
2026-08-15 02:46:20 +0530

    Time: 13 ms (73.64%), Space: 48.9 MB (6.63%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] nextGreaterElements(int[] arr) {
         int n = arr.length;
-        int[] nge = new int[n];
+        int[] nge = new int[n];//next  greater element
         Stack<Integer> st = new Stack<>();
         for(int i =n-1; i >= 0; i--) {
             st.push(arr[i]);//pushed whole array in stack
         }
         for(int i =n-1; i >= 0; i--) {//now start compairing
-            while(st.size() > 0 && arr[i]>=st.peek()) st.pop();
+            while(st.size() > 0 && arr[i]>=st.peek()) st.pop();//if new element>peek , pop the peek
             if(st.size() == 0)nge[i] = -1;
-            else nge[i] = st.peek();
+            else nge[i] = st.peek();//if new element<peek...nge of that new is peek
             st.push(arr[i]);
         }
         return nge;
